7f9667bcfe73daf681a46a511d38a11c8614f918
AsilbekLS <asilbekmasrurov1221ls@gmail.com>
2023-10-02 09:58:57 +0500

    react

---
+++ shablon/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./app.css";
+import { Dars5 } from "./dars5/Dars5";
+// import { Dars4 } from "./dars4/Dars4";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Dars4/> */}
+      <Dars5 />
+    </div>
+  );
+}
+
+export default App;
